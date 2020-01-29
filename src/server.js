500ad32c8459c41248132f82f0d7cd305edf59da
Adrian Blanco <adrian.blanco@mobgen.com>
2020-01-29 11:26:48 +0100

    Added cors to the server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 import router from './router';
 
 const app = new Koa();
@@ -20,6 +21,7 @@ function handleError(err, context) {
   }
 }
 
+app.use(cors());
 app.use(bodyParser());
 app.use((context, next) => next().catch((err) => handleError(err, context)));
 app.use(router.routes());
